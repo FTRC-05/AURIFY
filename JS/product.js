98a1bac4eaca0a2f1ae6d4f4a99e340d556be0dc
AmangiJnr <sundayamangi@gmail.com>
2025-07-17 10:20:17 +0100

    PRODUCT PAGE CREATED

---
+++ JS/product.js
@@ -0,0 +1,101 @@
+const commodities = [
+    {
+        id : 1,
+        image: "",
+        productName: "Airpods Max",
+        Price: 450000,
+    },
+    {
+        id : 2,
+        image: "",
+        productName: "Airpods Max",
+        Price: 450000,
+    },
+    {
+        id : 3,
+        image: "",
+        productName: "Airpods Max",
+        Price: 450000,
+    },
+    {
+        id : 4,
+        image: "",
+        productName: "Airpods Max",
+        Price: 450000,
+    },
+    {
+        id : 5,
+        image: "",
+        productName: "Airpods Max",
+        Price: 450000,
+    },
+    {
+        id : 6,
+        image: "",
+        productName: "Airpods Max",
+        Price: 450000,
+    },
+    {
+        id : 7,
+        image: "",
+        productName: "Airpods Max",
+        Price: 450000,
+    },
+    {
+        id : 8,
+        image: "",
+        productName: "Airpods Max",
+        Price: 450000,
+    },
+    {
+        id : 9,
+        image: "",
+        productName: "Airpods Max",
+        Price: 450000,
+    },
+    {
+        id : 9,
+        image: "",
+        productName: "Airpods Max",
+        Price: 450000,
+    },
+    {
+        id : 10,
+        image: "",
+        productName: "Airpods Max",
+        Price: 450000,
+    },
+    
+    {
+        id : 11,
+        image: "",
+        productName: "Airpods Max",
+        Price: 450000,
+    },
+];
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const productsContainer = document.getElementById('products');
+
+    commodities.forEach(product => {
+        const productCard = document.createElement('div');
+        productCard.className = 'product-card';
+
+        const imagePath = product.image || '/img/ftprod3.png';
+
+
+        productCard.innerHTML = `
+        <div class="product-image">
+                    <img src="${imagePath}" alt="${product.productName}">
+                </div>
+                <div class="product-details">
+                    <h4>${product.productName}</h4>
+                    <p class="price">&#8358;${product.Price.toLocaleString()}</p>
+                    <button class="add-to-cart">Add to Cart</button>
+                </div>
+        `;
+        productsContainer.appendChild(productCard);
+    })
+    });
+
